d94227fa74d2ba6bee17b969de8dcb0b3015332d
ioluwamichael <73822899+ioluwamichael@users.noreply.github.com>
2021-04-02 09:37:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function displayContent()   {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -29,9 +28,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
+
   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
+
 }
